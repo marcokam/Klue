2af790fd46bb5c69e463f1f55dc60ba814d74f14
Marc Kam <marco.kam@gmail.com>
2020-02-07 14:48:42 -0500

    feat(photos): handle basic search and render

--- src/components/Masonry/Masonry.js
+++ src/components/Masonry/Masonry.js
@@ -3,12 +3,11 @@ import React from "react";
 
 
 export function Masonry({ children = [] }) {
-    console.log('children', children);
     return (
         <div className="masonry-container">
             {React.Children.map(children, child => {
                 return (
-                    <div className="w-100 dib bg-white tc">
+                    <div className="w-100 dib mv2 tc">
                         {React.cloneElement(child)}
                     </div>
                 );
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,20 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo, useCallback } from "react";
 import { useParams } from "react-router-dom";
+import { debounce } from "lodash-es";
+
 import { searchPhotos, apiStates } from "../../api";
 import { Masonry } from "../Masonry/Masonry";
 
-export function Search() {
-    const { searchTerm } = useParams();
+
+export function Search({ pageSize = 20 }) {
+    const { searchTerm: initialSearchTerm = "" } = useParams();
+    const [searchTerm, setSearchTerm] = useState(initialSearchTerm);
+    const dbSetSearchTerm = useMemo(() => debounce(setSearchTerm, 200), []);
     const [searchData, setSearchData] = useState({ apiState: apiStates.none, payload: {} });
     const { results: photos = [] } = searchData.payload;
+    const handleSearch = useCallback(event => {
+        const searchTerm = (event && event.target && event.target.value) || "";
+        dbSetSearchTerm(searchTerm);
+    }, [dbSetSearchTerm]);
 
     useEffect(() => {
-        if (!searchTerm) return;
+        if (!searchTerm) {
+            setSearchData(prev => ({ ...prev, payload: {} }));
+        };
 
         let cancelled = false;
-        
         setSearchData(prev => ({ ...prev, apiState: apiStates.fetching }));
-        searchPhotos({ query: searchTerm })
+        searchPhotos({ query: searchTerm, per_page: pageSize })
             .then((payload = {}) => {
                 if (cancelled) return;
                 setSearchData({ apiState: apiStates.fetched, payload });
@@ -28,18 +38,21 @@ export function Search() {
             cancelled = true;
         };
 
-    }, [searchTerm]);
+    }, [pageSize, searchTerm]);
 
 
     return (
-        <div className="search-photos-container">
-            <input defaultValue={searchTerm} />
+        <div className="w-100 flex flex-column items-center">
+            <div className="w-100 h3 flex items-center absolute bg-black-70 pv2 ph4 shadow-5 z-1" style={{ top: 0, position: "sticky" }}>
+                <img className="h2" src="https://klue.com/wp-content/themes/klue/assets-home2/img/logo-klue.svg" alt="Klue logo" />
+                <input className="input-reset ml3 pa2 br2 ba b--black-20 w5" defaultValue={searchTerm} placeholder="Search for a Photo" onChange={handleSearch} />
+            </div>
             <Masonry>
                 {photos.map((photo = {}) => {
                     const { id = "", urls = {}, alt_description = "", description = "" } = photo;
                     const altText = alt_description || description || `photo`;
                     return (urls.thumb &&
-                        <img key={id} src={urls.thumb} alt={altText} />
+                        <img key={id} className="grow" src={urls.thumb} alt={altText} />
                     );
                 })}
             </Masonry>
